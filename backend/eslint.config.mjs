98976a822f600f182cd5d25fb7be4b3d61ad281e
dinesh <dinesh.colorwhistle@gmail.com>
2026-06-10 12:31:22 +0530

    lint error modified

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -34,6 +34,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-unsafe-return': 'off',
       '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/require-await': 'off',
       "prettier/prettier": ["error", { endOfLine: "auto" }],
     },
   },
